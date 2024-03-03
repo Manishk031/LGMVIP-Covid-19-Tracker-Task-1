9afa2dfa6a6222f8654fb87736ecd4c0d97650f9
Manishk031 <khandelwalmanish582@gmail.com>
2024-03-04 03:10:57 +0530

    adapter

--- app/src/main/java/com/zoro/covid_19tracker/StateAdapter.kt
+++ app/src/main/java/com/zoro/covid_19tracker/StateAdapter.kt
@@ -1,7 +1,9 @@
 package com.zoro.covid_19tracker
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class StateAdapter (private val stateList: List<model>) :
@@ -10,18 +12,29 @@ class StateAdapter (private val stateList: List<model>) :
 
     class StateViewHolder(itemView:View) :RecyclerView.ViewHolder(itemView){
 
+        val stateName:TextView = itemView.findViewById(R.id.txt_state)
+        val stateCases:TextView= itemView.findViewById(R.id.state_cases)
+        val stateRecovered:TextView= itemView.findViewById(R.id.state_recovered)
+        val stateDeaths:TextView= itemView.findViewById(R.id.state_deaths)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StateViewHolder {
-        TODO("Not yet implemented")
+      val itemView = LayoutInflater.from(parent.context).inflate(R.layout.state_rv_item,parent,false)
+        return StateViewHolder(itemView)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+       return stateList.size
     }
 
     override fun onBindViewHolder(holder: StateViewHolder, position: Int) {
-        TODO("Not yet implemented")
+       val stateData = stateList[position]
+        holder.stateCases.text = stateData.cases.toString()
+        holder.stateRecovered.text = stateData.recovered.toString()
+        holder.stateName.text = stateData.state.toString()
+        holder.stateDeaths.text = stateData.deaths.toString()
+
     }
 
 }
